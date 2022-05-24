d21dda43b8df785fe38642eacecd1d42a65cd005
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-24 11:17:32 -0400

    Adds CharacterModel and registers xib

---
+++ BreakingBad/Model/CharacterModel.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterModel.swift
+//  BreakingBad
+//
+//  Created by Sean Murphy on 5/24/22.
+//
+
+import Foundation
+import UIKit
+
+struct CharacterModel: Codable {
+    let char_id: Int
+    let image: String
+    let name: String
+    let birthday: Date
+    let occupation: [String]
+    let status: String
+    let nickname: String
+    let apperance: [Int]
+    let portrayed: String
+    let category: String
+}
--- BreakingBad/View/CharacterTableVC.swift
+++ BreakingBad/View/CharacterTableVC.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class CharacterTableVC: UITableViewController {
     
+    var characterArray = [CharacterInfo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "CharacterCell")
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
 }
